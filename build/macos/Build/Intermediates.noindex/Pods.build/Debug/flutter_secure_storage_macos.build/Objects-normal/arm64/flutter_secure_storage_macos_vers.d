652356e54930a53d5f66edef6b0321ccacf7cfb8
Rausch Barna <>
2025-05-27 06:58:00 +0200

    remove build folder

--- build/macos/Build/Intermediates.noindex/Pods.build/Debug/flutter_secure_storage_macos.build/Objects-normal/arm64/flutter_secure_storage_macos_vers.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/admin/Documents/test_application/test_application/build/macos/Build/Intermediates.noindex/Pods.build/Debug/flutter_secure_storage_macos.build/module.modulemap \
-  /Users/admin/Documents/test_application/test_application/build/macos/Build/Intermediates.noindex/Pods.build/Debug/flutter_secure_storage_macos.build/DerivedSources/flutter_secure_storage_macos_vers.c
